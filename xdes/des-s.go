1335d75383c20e41943e1cf472646bdfb6c3f79f
desktop-computer-AMDR5 <AMDRyzen5-5600G@deepin20.9>
2024-04-18 21:26:14 +0800

    pboc mac string

--- xdes/des-s.go
+++ xdes/des-s.go
@@ -2,6 +2,7 @@ package xdes
 
 import (
 	"encoding/hex"
+	"fmt"
 	"strings"
 )
 
@@ -38,5 +39,40 @@ func GetSubKey_S(key, dvs string) (out string, err error) {
 	}
 	out = hex.EncodeToString(bytOut)
 	out = strings.ToUpper(out)
+	return
+}
+
+func PbocMac_S(key, iv, data string) (sout string, err error) {
+	if len(key) != 32 {
+		err = fmt.Errorf("key invalid")
+		return
+	}
+	if len(iv) != 16 {
+		err = fmt.Errorf("iv invalid")
+		return
+	}
+	if len(data)%16 != 0 {
+		err = fmt.Errorf("data invalid, must be a multiple of 8")
+		return
+	}
+	bytKey, err := hex.DecodeString(key)
+	if err != nil {
+		return
+	}
+	bytIv, err := hex.DecodeString(iv)
+	if err != nil {
+		return
+	}
+	bytData, err := hex.DecodeString(data)
+	if err != nil {
+		return
+	}
+
+	bytMac, err := PbocMac(bytKey, bytIv, bytData, 4)
+	if err != nil {
+		return
+	}
+	sout = hex.EncodeToString(bytMac)
+	sout = strings.ToUpper(sout)
 	return
 }
